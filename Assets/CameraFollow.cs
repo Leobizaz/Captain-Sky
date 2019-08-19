4a0d3cb2a1b45892fd3e81c1fe8478977d6f0001
Leobizaz <leobizaz12@gmail.com>
2019-08-19 15:10:24 -0300

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector2 limits = new Vector2(5, 3);
+    public Vector3 offset = Vector3.zero;
+    public float smoothTime;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            transform.localPosition = offset;
+        }
+        FollowTarget(target);
+        
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 localPos = transform.localPosition;
+        transform.localPosition = new Vector3(Mathf.Clamp(localPos.x, -limits.x, limits.x), Mathf.Clamp(localPos.y, -limits.y, limits.y), localPos.z);
+    }
+
+    public void FollowTarget(Transform t)
+    {
+        Vector3 localPos = transform.localPosition;
+        Vector3 targetLocalPos = t.transform.localPosition;
+        transform.localPosition = Vector3.SmoothDamp(localPos, new Vector3(targetLocalPos.x + offset.x, targetLocalPos.y + offset.y, localPos.z), ref velocity, smoothTime);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(new Vector3(-limits.x, -limits.y, transform.position.z), new Vector3(limits.x, -limits.y, transform.position.z));
+        Gizmos.DrawLine(new Vector3(-limits.x, limits.y, transform.position.z), new Vector3(limits.x, limits.y, transform.position.z));
+        Gizmos.DrawLine(new Vector3(-limits.x, -limits.y, transform.position.z), new Vector3(-limits.x, limits.y, transform.position.z));
+        Gizmos.DrawLine(new Vector3(limits.x, -limits.y, transform.position.z), new Vector3(limits.x, limits.y, transform.position.z));
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.Rendering.PostProcessing;
+using DG.Tweening;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float horizontalSpeed = 10;
+    public float progressionSpeed = 6;
+    public float lookSpeed = 5f;
+    public GameObject modelo;
+    public Transform aimTarget;
+    public CinemachineDollyCart dolly;
+    public Transform cameraParent;
+    public GameObject cursor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetSpeed(progressionSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Mouse X");
+        float y = Input.GetAxis("Mouse Y");
+
+        MovimentoLocal(x, y, horizontalSpeed);
+        RotationLook(x, y, lookSpeed);
+        Inclinada(modelo.transform, x, 45, 0.1f);
+
+        if (Input.GetButtonDown("Fire3"))
+            Boost(true);
+
+        if (Input.GetButtonUp("Fire3"))
+            Boost(false);
+
+        if (Input.GetButtonDown("Fire1"))
+            Break(true);
+
+        if (Input.GetButtonUp("Fire1"))
+            Break(false);
+
+    }
+
+    void MovimentoLocal(float x, float y, float speed)
+    {
+        transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+        ClampPosition();
+    }
+
+    void ClampPosition()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Boundary em relação a camera
+
+        pos.x = Mathf.Clamp(pos.x, 0.1f, 0.9f);
+        pos.y = Mathf.Clamp(pos.y, 0.1f, 0.9f);
+
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+    void RotationLook(float x, float y, float speed)
+    {
+        aimTarget.parent.position = Vector3.zero;
+        aimTarget.localPosition = new Vector3(x, y, 1);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * speed * Time.deltaTime);
+    }
+
+    void Inclinada(Transform target, float eixo, float limite, float lerpTime)
+    {
+        Vector3 targetEulerAngles = target.localEulerAngles;
+        target.localEulerAngles = new Vector3(targetEulerAngles.x, targetEulerAngles.y, Mathf.LerpAngle(targetEulerAngles.z, -eixo * limite, lerpTime));
+    }
+
+    void SetSpeed(float x)
+    {
+        dolly.m_Speed = x;
+    }
+
+    void SetCameraZoom(float zoom, float duration)
+    {
+        cameraParent.DOLocalMove(new Vector3(0, 0, zoom), duration);
+    }
+
+    void Break(bool state)
+    {   
+        float speed = state ? progressionSpeed / 3 : progressionSpeed;
+        float zoom = state ? 3 : 0;
+
+        DOVirtual.Float(dolly.m_Speed, speed, .15f, SetSpeed);
+        SetCameraZoom(zoom, .4f);
+    }
+
+    void Boost(bool state)
+    {
+        float speed = state ? progressionSpeed * 2 : progressionSpeed;
+        float zoom = state ? -7 : 0;
+
+        DOVirtual.Float(dolly.m_Speed, speed, .15f, SetSpeed);
+        SetCameraZoom(zoom, .4f);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(aimTarget.position, 0.5f);
+        Gizmos.DrawSphere(aimTarget.position, 0.15f);
+    }
+}
