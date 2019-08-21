b56dc81589ef16bd01df1a6fe1b0b838b52ba9ba
GuiSR388 <guicrilopez@users.noreply.github.com>
2019-08-21 14:18:32 -0300

    + particulas e inimigo imortal

---
+++ Assets/Scripts/Enemys/MovingBird.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBird : MonoBehaviour
+{
+    float horizontalSpeed;
+    float verticalSpeed;
+    float amplitude;
+
+    private Vector3 tempPosition;
+
+
+    void Start()
+    {
+        tempPosition = transform.position;
+         horizontalSpeed = Random.Range(0.1f, 0.3f);
+         verticalSpeed = Random.Range(1f, 3f);
+         amplitude = Random.Range(2f, 4f);
+    }
+
+    void FixedUpdate()
+    {
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+        transform.position = tempPosition;
+        Object.Destroy(this.gameObject, 10.0f);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Shoot")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
