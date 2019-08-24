c4770455c60d90ea0919274a21f6f076ad3a1ab0
Leobizaz <leobizaz12@gmail.com>
2019-08-24 15:19:45 -0300

    Sistema de dialogos

---
+++ Assets/InstantiateDialogo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstantiateDialogo : MonoBehaviour
+{
+    public GameObject dialogo;
+    public Canvas canvas;
+    public float lifeTime;
+    public Sprite icone;
+    public string nome;
+    public string texto;
+    AudioSource audio;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject dialogocfg = Instantiate(dialogo) as GameObject;
+            dialogocfg.transform.SetParent(canvas.transform, false);
+            SpawnAnimation cfg = dialogocfg.GetComponent<SpawnAnimation>();
+            cfg.lifeTime = lifeTime;
+            cfg.icone.sprite = icone;
+            cfg.name = nome;
+            cfg.text = texto;
+            audio.Play();
+
+        }
+    }
+}
---
+++ Assets/SpawnAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class SpawnAnimation : MonoBehaviour
+{
+    public TextMeshProUGUI nome;
+    public TextMeshProUGUI mensagem;
+    public Image icone;
+    public float lifeTime;
+    public string text;
+    public string name;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Invoke("Die", lifeTime);
+        nome.text = name;
+        mensagem.text = text;
+    }
+
+    void Die()
+    {
+        anim.Play("PopOut");
+        Invoke("RIP", 1f);
+    }
+
+    void RIP()
+    {
+        Destroy(gameObject);
+    }
+
+    
+}
