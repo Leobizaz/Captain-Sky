8fe596abc5ee5f1118937baed0bdff0ff6509957
Leobizaz <leobizaz12@gmail.com>
2019-08-24 12:21:55 -0300

    Render Pipeline + enemy deployment

---
+++ Assets/ActivateChild.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateChild : MonoBehaviour
+{
+    public GameObject child;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            child.SetActive(true);
+        }
+    }
+
+}
