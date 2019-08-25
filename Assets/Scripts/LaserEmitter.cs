7f1f013ca789cc7009292c885e69daccd2c190e4
Leobizaz <leobizaz12@gmail.com>
2019-08-25 13:42:23 -0300

    Post process e progresso no level 1

---
+++ Assets/Scripts/LaserEmitter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserEmitter : MonoBehaviour
+{
+    public GameObject receiver;
+    public LineRenderer laserbeam;
+    public float laserDistance;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        laserbeam.SetPosition(0, gameObject.transform.position);
+        laserbeam.SetPosition(1, receiver.transform.position);
+
+        int layermask = 1 << 9;
+        layermask = ~layermask;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, laserDistance, layermask))
+        {
+            receiver.transform.LookAt(gameObject.transform);
+            receiver.transform.position = hit.point;
+        }
+        else
+        {
+            receiver.transform.position = new Vector3(receiver.transform.position.x, receiver.transform.position.y, laserDistance);
+            receiver.transform.LookAt(gameObject.transform);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class SpawnAnimation : MonoBehaviour
+{
+    public TextMeshProUGUI nome;
+    public TextMeshProUGUI mensagem;
+    public Image icone;
+    public float lifeTime;
+    public string text;
+    public string name;
+    Animator anim;
+
+    void Start()
+    {
+        InstantiateDialogo.dialogoPlaying = true;
+        anim = GetComponent<Animator>();
+        Invoke("Die", lifeTime);
+        nome.text = name;
+        mensagem.text = text;
+    }
+
+    void Die()
+    {
+        anim.Play("PopOut");
+        Invoke("RIP", 1f);
+        InstantiateDialogo.dialogoPlaying = false;
+    }
+
+    void RIP()
+    {
+        Destroy(gameObject);
+    }
+
+    
+}
