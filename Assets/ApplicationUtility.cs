31fd3e09e0facf5fdd7313fd988902462dfdb5f8
leonardo <leobizaz12@gmail.com>
2019-09-09 10:30:08 -0300

    Menu finalizado

---
+++ Assets/ApplicationUtility.cs
@@ -0,0 +1,18 @@
+﻿ #if UNITY_EDITOR
+ using UnityEditor;
+ using UnityEditorInternal;
+ #endif
+ using UnityEngine;
+ public static class ApplicationUtility
+ {
+     // A generic "is focused" method that also returns true when the Unity application window
+     // is active, regardless the specific editor window that is focused.
+     public static bool IsActivated()
+     {
+         #if UNITY_EDITOR
+         return InternalEditorUtility.isApplicationActive;
+         #else
+         return Application.isFocused;
+         #endif
+     }
+ }
