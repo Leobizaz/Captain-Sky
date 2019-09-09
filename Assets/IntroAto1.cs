486531ae0fa84accd8a24ab22d6a85cac588753e
leonardo <leobizaz12@gmail.com>
2019-09-09 12:22:58 -0300

    Cutscene de intro pog

--- Assets/IntroAto1.cs
+++ Assets/IntroAto1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 using UnityEngine.Rendering.PostProcessing;
 
 public class IntroAto1 : MonoBehaviour
@@ -8,7 +9,12 @@ public class IntroAto1 : MonoBehaviour
     public GameObject gameplay;
     public GameObject canvas;
     public GameObject timeline;
+    public GameObject musica;
     public GameObject maincamera;
+
+    public GameObject fadeBlack;
+
+    public VideoPlayer videoPlayer;
 
     void Start()
     {
@@ -16,10 +22,23 @@ public class IntroAto1 : MonoBehaviour
         gameplay.SetActive(false);
         canvas.SetActive(false);
         timeline.SetActive(false);
+        musica.SetActive(false);
     }
 
     void Update()
     {
-        
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        fadeBlack.SetActive(true);
+        Debug.Log("Cabo video");
+        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
+        maincamera.GetComponent<PostProcessLayer>().enabled = true;
+        //gameplay.SetActive(true);
+        canvas.SetActive(true);
+        //timeline.SetActive(true);
+        //musica.SetActive(true);
     }
 }
