2196f3eb2a344919b6f6c7624a2179bb9154be4c
Leobizaz <leobizaz12@gmail.com>
2019-09-12 23:16:21 -0300

    Tuneis ato 3

---
+++ Assets/Ato3.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ato3 : MonoBehaviour
+{
+    public bool startWithFog;
+
+    void Start()
+    {
+        if (startWithFog)
+        {
+            RenderSettings.fog = true;
+        }
+            
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TurnOffLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffLight : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RenderSettings.fogColor = new Color(0, 0, 0, 0);
+            RenderSettings.fogStartDistance = 100;
+            RenderSettings.fogEndDistance = 600;
+        }
+    }
+}
